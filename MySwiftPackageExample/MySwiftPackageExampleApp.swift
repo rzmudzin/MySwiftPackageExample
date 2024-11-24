3d935741d0405023222ff0bf2e26007573f65037
rob <rob@rob.com>
2024-11-24 16:24:03 -0700

    Initial Commit

---
+++ MySwiftPackageExample/MySwiftPackageExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  MySwiftPackageExampleApp.swift
+//  MySwiftPackageExample
+//
+//  Created by Rob Zmudzinshi on 11/24/24.
+//
+
+import SwiftUI
+import MySwiftPackage
+
+@main
+struct MySwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            SwiftUIView()
+        }
+    }
+}
